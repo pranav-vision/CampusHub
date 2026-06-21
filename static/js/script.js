4c1b066060c933fa2c1f178901167a54c3f059f5
9371952461 <167550278+9371952461@users.noreply.github.com>
2026-06-21 15:43:06 +0530

    Added frontend pages

---
+++ static/js/script.js
@@ -0,0 +1,13 @@
+// ===============================
+// CampusHub JavaScript
+// ===============================
+
+console.log("CampusHub Loaded Successfully");
+
+window.onload = function () {
+    console.log("Welcome to CampusHub");
+};
+
+function uploadSuccess() {
+    alert("Resource Uploaded Successfully!");
+}
